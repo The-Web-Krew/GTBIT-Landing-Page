9563e154cc1db49bc2dcbf1d18cd254c7dfee7cc
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2022-08-27 01:16:56 +0530

    Proud

--- scrypt.js
+++ scrypt.js
@@ -1,116 +1,173 @@
-
 // ================= Navbar===================//
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
-    navLinks.classList.toggle("open");
-    links.forEach(link => {
-        link.classList.toggle("fade");
-    });
+hamburger.addEventListener("click", () => {
+  //Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 
-    //Hamburger Animation
-    hamburger.classList.toggle("toggle");
+  //Hamburger Animation
+  hamburger.classList.toggle("toggle");
 });
 
-
 // ================= Counter===================//
-
 
 const counters = document.querySelectorAll(".counter");
 
 counters.forEach((counter) => {
-counter.innerText = "0";
+  counter.innerText = "0";
 
-const updateCounter = () => {
-  const target = +counter.getAttribute("data-target");
-  const c = +counter.innerText;
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
 
-  const increment = target / 500;
+    const increment = target / 500;
 
-  if (c < target) {
-    counter.innerText = `${Math.ceil(c + increment)}`;
-    setTimeout(updateCounter, 1);
-  } else {
-    counter.innerText = target;
-  }
-};
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  };
 
-updateCounter();
+  updateCounter();
 });
 
-// ================= Start up===================//
 
-$('.owl-carousel_1').owlCarousel({
-  loop:true,
-  nav:false,
-  autoplay:true,
-  autoplayTimeout:5000,
-  autoplayHoverPause:'true',
-  margin:0,
-  dots:true,
-  center:true,
-  responsive:{
-      0:{
-          items:1
-      },
-      700:{
-          items:1
-      },
-      1100:{
-          items:3
-      }
-  }
-})
+// ================= proud_moment ==================//
 
-$('.owl-carousel').on('translate.owl.carousel', function(e) {
+$(".proud_moment").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: "true",
+  margin: 0,
+  dots: true,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    700: {
+      items: 1,
+    },
+    1100: {
+      items: 5,
+    },
+  },
+});
+
+$(".proud_moment").on("translate.owl.carousel", function (e) {
   var index = e.item.index;
-  $('.card-alumni').eq(index - 1).addClass('side');
-  $('.card-alumni').eq(index + 1).addClass('side');
-  $('.card-alumni').eq(index - 1).removeClass('main');
-  $('.card-alumni').eq(index + 1).removeClass('main');
-  $('.card-alumni').eq(index).removeClass('side');
-  $('.card-alumni').eq(index).addClass('main');
-});
+  // +_2
+  $(".batch")
+  .eq(index - 2)
+  .addClass("fade_2");
 
+$(".batch")
+  .eq(index + 2)
+  .addClass("fade_2");
 
+$(".batch")
+  .eq(index - 2)
+  .removeClass("main fade_1");
+$(".batch")
+  .eq(index + 2)
+  .removeClass("main fain_1");
 
 
   
+  // +- 1
+  $(".batch")
+    .eq(index - 1)
+    .addClass("fade_1");
+
+  $(".batch")
+    .eq(index + 1)
+    .addClass("fade_1");
+
+  $(".batch")
+    .eq(index - 1)
+    .removeClass("main fade_2");
+  $(".batch")
+    .eq(index + 1)
+    .removeClass("main fade_2");
+
+// MAIN
+  $(".batch").eq(index).removeClass("fade_1");
+  $(".batch").eq(index).addClass("main");
+});
+
+
+
+// ================= Start up===================//
+
+$(".owl-carousel_1").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: "true",
+  margin: 0,
+  dots: true,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    700: {
+      items: 1,
+    },
+    1100: {
+      items: 3,
+    },
+  },
+});
+
 // ================= Owl-Carousel===================//
 
-$('.owl-carousel_2').owlCarousel({
-    loop:true,
-    nav:false,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:'true',
-    margin:0,
-    dots:true,
-    center:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        700:{
-            items:1
-        },
-        1100:{
-            items:3
-        }
-    }
-})
+$(".owl-carousel_2").owlCarousel({
+  loop: true,
+  nav: false,
+  autoplay: true,
+  autoplayTimeout: 5000,
+  autoplayHoverPause: "true",
+  margin: 0,
+  dots: true,
+  center: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    700: {
+      items: 1,
+    },
+    1100: {
+      items: 3,
+    },
+  },
+});
 
- $('.owl-carousel').on('translate.owl.carousel', function(e) {
-    var index = e.item.index;
-    $('.card-alumni').eq(index - 1).addClass('side');
-    $('.card-alumni').eq(index + 1).addClass('side');
-    $('.card-alumni').eq(index - 1).removeClass('main');
-    $('.card-alumni').eq(index + 1).removeClass('main');
-    $('.card-alumni').eq(index).removeClass('side');
-    $('.card-alumni').eq(index).addClass('main');
-  });
-
-
+$(".owl-carousel").on("translate.owl.carousel", function (e) {
+  var index = e.item.index;
+  $(".card-alumni")
+    .eq(index - 1)
+    .addClass("side");
+  $(".card-alumni")
+    .eq(index + 1)
+    .addClass("side");
+  $(".card-alumni")
+    .eq(index - 1)
+    .removeClass("main");
+  $(".card-alumni")
+    .eq(index + 1)
+    .removeClass("main");
+  $(".card-alumni").eq(index).removeClass("side");
+  $(".card-alumni").eq(index).addClass("main");
+});
